b1c2bedf7817badbce365abc9c73761cc1a3c827
M1kee <michael.nunezb@outlook.com>
2019-07-16 18:05:07 -0400

    Ask to the user the folder path to work with. Add CancellationException to stop the program. Add Assembly auto increment versioning.

---
+++ WordToPDF/Exceptions/CancellationException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordToPDF.Exceptions
+{
+    public class CancellationException : Exception
+    {
+        public string Method { get; set; }
+        private string _message { get; set; }
+
+        public override string Message
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(Method))
+                    return _message;
+                else
+                    return $"[{Method}] - {_message}";
+            }
+        }
+
+        public CancellationException(string method, string message) : base(message) {
+            Method = method;
+            _message = message;
+        }
+        public CancellationException(string message) : base(message) {
+            _message = message;
+        }
+        public CancellationException(string message, Exception innerException) : base(message, innerException) {
+            _message = message;
+        }
+    }
+}
--- WordToPDF/Program.cs
+++ WordToPDF/Program.cs
@@ -4,18 +4,22 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Microsoft.Office.Interop.Word;
+using System.Windows.Forms;
+using WordToPDF.Exceptions;
 
 namespace WordToPDF
 {
     class Program
     {
-        public static Application AppWord { get; set; }
+        public static Microsoft.Office.Interop.Word.Application AppWord { get; set; }
+        public static string ExportPath { get; set; }
 
+        [STAThread]
         static void Main(string[] args)
         {
             Console.WriteLine("*-------------------------------------------------------*");
             Console.WriteLine($"> Microsoft Word to PDF");
-            Console.WriteLine($"> Versión: { FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).FileVersion }");
+            Console.WriteLine($"> Versión: { FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).ProductVersion }");
             Console.WriteLine($"> Inicio: { DateTime.Now.ToString($"dd/MM/yyyy HH:mm:ss") }");
             Console.WriteLine("*-------------------------------------------------------*");
 
@@ -24,10 +28,11 @@ namespace WordToPDF
                 // used as auxiliar to write errors on log and console
                 string message = string.Empty;
 
+                // used as default base path
                 string directoryToProcess = Directory.GetCurrentDirectory();
 
                 // get files to process
-                var files = GetFilePaths(directoryToProcess);
+                var files = GetFilePaths(false, directoryToProcess);
 
                 // get the error when no files detected
                 if (!files.Any())
@@ -37,7 +42,7 @@ namespace WordToPDF
                 }
                 else
                 {
-                    AppWord = new Application();
+                    AppWord = new Microsoft.Office.Interop.Word.Application();
                     Console.WriteLine($"> Cantidad de archivos a procesar: { files.Count }");
                     Console.WriteLine("*-------------------------------------------------------*");
                     int correctlyExported = 0;
@@ -66,12 +71,19 @@ namespace WordToPDF
                     Console.WriteLine($"Archivos exportados: {correctlyExported} de {files.Count}");
                 }
             }
+            catch (CancellationException cEx)
+            {
+                Console.WriteLine($"** {cEx.Message}");
+            }
             catch (Exception ex)
             {
                 Console.WriteLine($"** Ocurrió un error en el proceso. Error: { ex.Message }.{ ex.InnerException?.Message }**");
             }
             finally
             {
+                // close all open documents
+                if (AppWord?.Documents?.Count > 0)
+                    AppWord?.Documents?.Close();
                 // close word app
                 AppWord?.Quit();
                 // show end of the process
@@ -96,9 +108,15 @@ namespace WordToPDF
             Document wordDocument = null;
             try
             {
+                string exportPath = GetExportPath(false);
+
                 // convert the file to pdf
                 wordDocument = AppWord.Documents.Open(file);
-                wordDocument.ExportAsFixedFormat($"{Path.GetDirectoryName(file)}\\{fileName}.pdf", WdExportFormat.wdExportFormatPDF);
+                wordDocument.ExportAsFixedFormat($"{exportPath}\\{fileName}.pdf", WdExportFormat.wdExportFormatPDF);
+            }
+            catch (CancellationException)
+            {
+                throw;
             }
             catch (Exception ex)
             {
@@ -114,16 +132,67 @@ namespace WordToPDF
         }
 
         /// <summary>
-        /// Get files from given basePath. If no base path, use the current directory.
+        /// Get export path, If the user don't select, use default export path
+        /// </summary>
+        /// <param name="useDefaultExportPath"></param>
+        /// <returns>Export Path or an CancellationException</returns>
+        private static string GetExportPath(bool useDefaultExportPath)
+        {
+            if (!string.IsNullOrWhiteSpace(ExportPath))
+                return ExportPath;
+
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog
+            {
+                Description = $"Seleccione la ruta desde donde se guardarán los archivos transformados a PDF.", 
+                RootFolder = Environment.SpecialFolder.Desktop
+            };
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                ExportPath = folderBrowserDialog.SelectedPath;
+            }
+            else if (useDefaultExportPath)
+            {
+                ExportPath = Path.Combine(Directory.GetCurrentDirectory(), "exports");
+                Console.WriteLine($"> El usuario no seleccionó ninguna ruta, se utilizará la ruta por defecto: \"{ ExportPath }\"");
+            }
+            else
+            {
+                throw new CancellationException("GetExportPath", $"El usuario canceló su petición.");
+            }
+
+            return ExportPath;
+        }
+
+        /// <summary>
+        /// Get files from given default basePath. If the user don't select, use default basePath
         /// </summary>
         /// <param name="basePath"></param> 
-        /// <returns></returns>
-        private static IList<string> GetFilePaths(string basePath)
+        /// <returns>IList<string> with files on selected folder.</returns>
+        private static IList<string> GetFilePaths(bool useBasePath, string basePath)
         {
-            if (string.IsNullOrWhiteSpace(basePath))
-                basePath = Directory.GetCurrentDirectory();
+            IList<string> paths = new List<string>();
 
-            return Directory.GetFiles(basePath, "*.docx").ToList();
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog()
+            {
+                Description = $"Seleccione la ruta desde donde se obtendrán los archivos de Microsoft Word.",
+                RootFolder = Environment.SpecialFolder.Desktop
+            };
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                paths = Directory.GetFiles(folderBrowserDialog.SelectedPath, "*.doc*").ToList();
+            }
+            else if (useBasePath && !string.IsNullOrWhiteSpace(basePath))
+            {
+                Console.WriteLine($"> El usuario no seleccionó ninguna ruta, se utilizará la ruta por defecto: \"{ basePath }\"");
+                basePath = Directory.GetCurrentDirectory();
+                paths = Directory.GetFiles(basePath, "*.doc*").ToList();
+            }
+            else
+            {
+                throw new CancellationException("GetFilePaths", $"El usuario canceló su petición.");
+            }
+
+            return paths;
         }
     }
 }
